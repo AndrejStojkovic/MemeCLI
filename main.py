a8b3fdf2e32fb9345761102887bb9a13471546c4
Andrej Stojkovikj <40241148+AndrejStojkovic@users.noreply.github.com>
2022-01-30 01:24:00 +0100

    30% Done

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys, argparse
+import meme
+
+def main():
+    parser = argparse.ArgumentParser(description="Python CLI Meme Generator")
+
+    parser.add_argument("-i", "--input", required=True, type=str, help="Input file to generate meme from.")
+    parser.add_argument("-o", "--output", required=True, type=str, help="Output file name to generate meme.")
+    parser.add_argument("-t", "--type", required=True, type=int, help="Type of meme. (1 - 4)")
+    parser.add_argument("-s", "--string", required=True, type=str, nargs='+', help="Text/String to add to the meme.")
+    parser.add_argument("-f", "--font", type=str, help="Choose the font you want to use for the meme.")
+
+    args = parser.parse_args()
+    meme.gen_meme(args.input, args.output, args.type, args.string, args.font)
+
+if __name__ == "__main__":
+    if not sys.argv[1:]:
+        print("[Usage]: python {} -h (--help)".format(sys.argv[0]))
+    main()
---
+++ meme.py
@@ -0,0 +1,77 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def gen_meme(input_file, output_file, meme_type, m_text, f_name):
+    try:
+        image = Image.open('memes/' + input_file)
+    except:
+        return print("File not found or unreadable!")
+
+    width, height = image.size
+    output_file = 'memes/exported/' + output_file
+
+    meme_font = ''
+    font_name = 'arial.ttf'
+    font_size = int(width / 18)
+    text = ' '.join(m_text)
+
+    if meme_type == 3:
+        font_name = 'impact.ttf'
+        font_size = int(width / 12)
+
+    if f_name is not None:
+        font_name = f_name
+
+    meme_font = ImageFont.truetype("fonts/" + font_name, font_size)
+
+    if meme_type == 1:
+        type_one(width, height, image, text, meme_font, font_size, output_file)
+    elif meme_type == 2:
+        print("Meme type 2")
+    elif meme_type == 3:
+        type_three(width, height, image, text, meme_font, output_file)
+    elif meme_type == 4:
+        print("Meme type 4")
+    else:
+        print("[Error]: No such meme type.")
+
+    print("Successfully exported meme!")
+
+def type_one(width, height, image, text, meme_font, font_size, output_file):
+    padding = 30
+    textPadding = font_size + 2
+    new_height = height + textPadding + padding
+    result = Image.new(image.mode, (width, new_height), (255, 255, 255))
+    result.paste(image, (0, textPadding + padding))
+
+    meme_text = ImageDraw.Draw(result)
+    meme_text.text((0, 0), text, font=meme_font, fill=(0, 0, 0))
+
+    result.save(output_file)
+
+def type_three(width, height, image, text, meme_font, output_file):
+    top_text = text
+    bottom_text = ''
+
+    if ',' in text:
+        dtext = text.split(',')
+        top_text = dtext[0]
+        bottom_text = dtext[1]
+
+    meme_text = ImageDraw.Draw(image)
+
+    text_width, text_height = get_text_dim(top_text, meme_font)
+    center = (width / 2) - (text_width / 2)
+    meme_text.text((center, 0), top_text, font=meme_font, fill=(255, 255, 255), stroke_width=1, stroke_fill=(0, 0, 0))
+
+    if bottom_text != '':
+        text_width, text_height = get_text_dim(bottom_text, meme_font)
+        center = (width / 2) - (text_width / 2)
+        meme_text.text((center, height - text_height - 10), bottom_text, font=meme_font, fill=(255, 255, 255), stroke_width=1, stroke_fill=(0, 0, 0))
+
+    image.save(output_file)
+
+def get_text_dim(text_string, font):
+    ascent, descent = font.getmetrics()
+    text_width = font.getmask(text_string).getbbox()[2]
+    text_height = font.getmask(text_string).getbbox()[3] + descent
+    return (text_width, text_height)
